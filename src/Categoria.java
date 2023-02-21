977fc65edc8e62414a37b1eb39e0b0a930e8855a
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2023-02-20 22:43:28 -0500

    Initial commit

--- src/Categoria.java
+++ src/Categoria.java
@@ -1,19 +1,21 @@
 import java.util.ArrayList;
 
 public class Categoria {
-    private String code;
-    private String name;
-    private String desc;
+    public String code;
+    public String name;
+    public String desc;
+    public int tabs;
 
-    public Categoria(Categoria parentCategoria, String name, String code, String desc){
-        this.parentCategoria = parentCategoria;
+    public Categoria(Categoria parent, String name, String code, String desc, int tabs) {
+        this.parent = parent;
         this.code = code.replaceAll("\t", "");
         this.name = name.replaceAll("\t", "");
         this.desc = desc.replaceAll("\t", "");
+        this.tabs = tabs;
     }
     public Categoria(){};
-    public ArrayList<Producto> productos;
-    public ArrayList<Categoria> subCategorias;
+    public ArrayList<Producto> productos = new  ArrayList<Producto>();
+    public ArrayList<Categoria> subCategorias = new ArrayList<Categoria>();
 
-    private Categoria parentCategoria;
+    public Categoria parent;
 }
